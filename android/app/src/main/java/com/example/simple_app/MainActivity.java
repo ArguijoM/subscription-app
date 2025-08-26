603bfbdd1839114b477cb1d192267afef2562d6d
ArguijoM <arguijomanuel@gmail.com>
2025-08-26 11:07:23 -0600

    Problema con dispositivos fisicos

--- android/app/src/main/java/com/example/simple_app/MainActivity.java
+++ android/app/src/main/java/com/example/simple_app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.simple_app;
+
+import android.os.Bundle;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 
 public class MainActivity extends FlutterFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Forzar el theme correcto antes de inicializar Flutter
+        setTheme(R.style.NormalTheme);
+        super.onCreate(savedInstanceState);
+    }
 }
